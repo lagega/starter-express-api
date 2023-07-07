fea804b7edd850577cd14c4f3a5bcd58e745de5d
Gabriel <lage.gabriel@edu.unirio.br>
2023-07-07 00:27:02 -0300

    Add user and db methods

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import bodyParser from 'body-parser';
+import express from 'express';
+
+import { handleNewUser } from './src/user.js';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.all('/', async (req, res) => {
+  console.log('Just got a request!');
+  await handleNewUser(req, res);
+});
+
+app.listen(3000);
